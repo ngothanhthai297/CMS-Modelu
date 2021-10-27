cddbcd2194e2234db8d6528ea5c4ea5679d0de04
mytien0906 <thaitiendrm@gmail.com>
2021-10-27 13:55:51 +0700

    update content module

--- wp-content/themes/twentytwenty/template-parts/content.php
+++ wp-content/themes/twentytwenty/template-parts/content.php
@@ -11,14 +11,19 @@
  * @subpackage Twenty_Twenty
  * @since Twenty Twenty 1.0
  */
-
+$class = "";
+if (!is_single()) {
+	$class = 'danh-sach';
+}
 ?>
 
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class($class); ?> id="post-<?php the_ID(); ?>">
 
 	<?php
 
-	get_template_part('template-parts/entry-header');
+	if (is_single()) {
+		get_template_part('template-parts/entry-header');
+	}
 
 	if (!is_search()) {
 		get_template_part('template-parts/featured-image');
@@ -35,63 +40,87 @@
 				if (is_search() || !is_singular() && 'summary' === get_theme_mod('blog_content', 'full')) {
 					the_excerpt();
 				} else {
-					the_content(__('Continue reading', 'twentytwenty'));
+					if (is_single()) {
+						the_content(__('Continue reading', 'twentytwenty'));
+					} else {
+						$post = get_post();
+						$date = date('d', strtotime($post->post_date));
+						$month = date('m', strtotime($post->post_date));
+				?>
+						<div class="list_news">
+							<div class="list_new_view">
+								<div class="row">
+								<div class="col-md-12 top_news_block_desc">
+											<div class="row">
+												<div class="col-md-3 col-xs-3 topnewstime">
+													<span class="topnewsdate"><?php echo $date ?></span><br>
+													<span class="topnewsmonth">Tháng <?php echo $month ?></span><br>
+												</div>
+												<div class="col-md-9 col-xs-9 shortdesc">
+													<h4 class="entry-title">
+														<a href="<?php echo esc_url(get_permalink()) ?>"><?php echo $post->post_title ?></a>
+													</h4>
+													<p><?php echo substr($post->post_content, 0, 100);  ?><a href="<?php echo esc_url(get_permalink()) ?>">[...]</a></p>
+												</div>
+											</div>
+										</div>
+								</div>
+							</div>
+						</div>
+
+				<?php }
+				}
+				?>
+				<!-- .entry-content -->
+
+			</div><!-- .post-inner -->
+
+			<div class="section-inner">
+				<?php
+				wp_link_pages(
+					array(
+						'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__('Page', 'twentytwenty') . '"><span class="label">' . __('Pages:', 'twentytwenty') . '</span>',
+						'after'       => '</nav>',
+						'link_before' => '<span class="page-number">',
+						'link_after'  => '</span>',
+					)
+				);
+
+				edit_post_link();
+
+				// Single bottom post meta.
+				twentytwenty_the_post_meta(get_the_ID(), 'single-bottom');
+
+				if (post_type_supports(get_post_type(get_the_ID()), 'author') && is_single()) {
+
+					get_template_part('template-parts/entry-author-bio');
 				}
 				?>
 
-			</div>
-		</div>
+			</div><!-- .section-inner -->
 
-		<!-- .entry-content -->
+			<?php
 
-	</div><!-- .post-inner -->
+			if (is_single()) {
 
-	<div class="section-inner">
-		<?php
-		wp_link_pages(
-			array(
-				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__('Page', 'twentytwenty') . '"><span class="label">' . __('Pages:', 'twentytwenty') . '</span>',
-				'after'       => '</nav>',
-				'link_before' => '<span class="page-number">',
-				'link_after'  => '</span>',
-			)
-		);
+				get_template_part('template-parts/navigation');
+			}
 
-		edit_post_link();
-
-		// Single bottom post meta.
-		twentytwenty_the_post_meta(get_the_ID(), 'single-bottom');
-
-		if (post_type_supports(get_post_type(get_the_ID()), 'author') && is_single()) {
-
-			get_template_part('template-parts/entry-author-bio');
-		}
-		?>
-
-	</div><!-- .section-inner -->
-
-	<?php
-
-	if (is_single()) {
-
-		get_template_part('template-parts/navigation');
-	}
-
-	/*
+			/*
 	 * Output comments wrapper if it's a post, or if comments are open,
 	 * or if there's a comment number – and check for password.
 	 */
-	if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) {
-	?>
+			if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) {
+			?>
 
-		<div class="comments-wrapper section-inner">
+				<div class="comments-wrapper section-inner">
 
-			<?php comments_template(); ?>
+					<?php comments_template(); ?>
 
-		</div><!-- .comments-wrapper -->
+				</div><!-- .comments-wrapper -->
 
-	<?php
-	}
-	?>
+			<?php
+			}
+			?>
 
 </article><!-- .post -->
